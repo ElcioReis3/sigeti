832144690fb547efac416bab5353a121e43702db
Elcio Reis <elcio.reis024@gmail.com>
2026-05-06 17:21:29 -0300

    refactor: Atualizar novas autorizações no DashboardController

--- app/Controllers/Teach/DashboardController.php
+++ app/Controllers/Teach/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Controllers\Teach;
 
 use App\Core\Auth;
 use App\Core\Controller;
+use App\Core\Permission;
 use App\Models\Ticket;
 use App\Models\User;
 
@@ -13,7 +14,7 @@ class DashboardController extends Controller
     {
         parent::__construct("App");
 
-        Auth::requireRole(User::TEACH);
+        Auth::requirePermission(Permission::VIEW_REQUESTER_DASHBOARD);
     }
 
     public function index(): void
