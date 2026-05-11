cfd7918565b831116e1127c01a80393d427c1201
Elcio Reis <elcio.reis024@gmail.com>
2026-05-11 15:09:05 -0300

    feat: adicionar a pasta Admin e o aquivo DashboardController

--- app/Controllers/Admin/DashboardController.php
+++ app/Controllers/Admin/DashboardController.php
@@ -2,15 +2,18 @@
 
 namespace App\Controllers\Admin;
 
-use App\Core\AbstractModel;
-use App\Core\Auth;
 
-class DashboardController extends AbstractModel
+use App\Core\Auth;
+use App\Core\Controller;
+use App\Core\Permission;
+
+class DashboardController extends Controller
 {
     public function __construct()
     {
         parent::__construct("App");
 
+        Auth::requirePermission(Permission::VIEW_MANAGER_DASHBOARD);
     }
 
     public function index(): void
