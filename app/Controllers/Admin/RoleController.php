c92487a5b5fbff21e911114e6d3f97706e1b60fe
Elcio Reis <elcio.reis024@gmail.com>
2026-05-13 15:45:41 -0300

    feat: adicionar o método index ao RoleController

---
+++ app/Controllers/Admin/RoleController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controllers\Admin;
+
+use App\Core\Auth;
+use App\Core\Controller;
+use App\Core\Message;
+use App\Core\Permission;
+use App\Models\Category;
+use App\Models\Role\Role;
+use App\Models\School;
+use App\Models\SchoolUser;
+use App\Models\Ticket\Ticket;
+use App\Models\User;
+
+class RoleController extends Controller
+{
+    public function __construct()
+    {
+        parent::__construct("App");
+
+        Auth::requirePermission(Permission::VIEW_ROLES);
+    }
+
+    public function index(): void
+    {
+        Auth::requirePermission(Permission::VIEW_ROLES);
+        $roles = Role::all();
+
+        echo $this->view->render("admin/role/index", [
+            'roles' => $roles
+        ]);
+    }
+
+
+}
